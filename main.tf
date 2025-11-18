ccebadc9abb855432fc81aadfffd67c21a57c147
Praveenmartha121 <praveenmartha121@gmail.com>
2025-11-18 20:11:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "web_server_as" {
     image_id           = "ami-0cae6d6fe6048ca2c"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "minimajor"
+    key_name = "PRAVEENMARTHA"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-097b4975cf1130493", "subnet-01ee7e5094f09f366"]
+     subnets = ["subnet-0f1522f0158897042", "subnet-0bfbfa701388ab548"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
